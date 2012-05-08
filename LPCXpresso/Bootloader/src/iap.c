fbf9f3ab57954bb5821c673128f50f278c156ce0
Harmjan Treep <harmjan.treep@gmail.com>
2012-05-08 12:20:25 +0200

    Draft of the IAP drivers using LPC17xx-Drivers
    The IAP drivers are implemented using the LPC17xx-Drivers.
    Later on I am plannign on replacing them with our own code.
    The getDeviceSerial function has been tested, the rest not.

---
+++ LPCXpresso/Bootloader/src/iap.c
@@ -0,0 +1,104 @@
+/**
+ *     ______       _       ____  _____   ______                    _   __                       __
+ *   .' ___  |     / \     |_   \|_   _| |_   _ \                  / |_[  |                     |  ]
+ *  / .'   \_|    / _ \      |   \ | |     | |_) |   .--.    .--. `| |-'| |  .--.   ,--.    .--.| | .---.  _ .--.
+ *  | |          / ___ \     | |\ \| |     |  __'. / .'`\ \/ .'`\ \| |  | |/ .'`\ \`'_\ : / /'`\' |/ /__\\[ `/'`\]
+ *  \ `.___.'\ _/ /   \ \_  _| |_\   |_   _| |__) || \__. || \__. || |, | || \__. |// | |,| \__/  || \__., | |
+ *   `.____ .'|____| |____||_____|\____| |_______/  '.__.'  '.__.' \__/[___]'.__.' \'-;__/ '.__.;__]'.__.'[___]
+ *
+ * ===============================================================================================================
+ *
+ * The functions to deal with the IAP API.
+ *
+ * @author Chiel de Roest <M.A.deRoest@student.tudelft.nl> and Harmjan Treep <harmjan.treep@gmail.com>
+ */
+
+#include "iap.h"
+#include "lpc17xx_iap.h"
+
+IAP iapFunction=(IAP)IAP_LOCATION;
+
+/**
+ * Prepare the flash for writing.
+ *
+ * @param[in] sector The sector to prepare for writing.
+ */
+void prepareFlash( uint8_t sector ) {
+	iap_write_prepare( sector, sector );
+}
+
+/**
+ * Check if a sector of the flash is blank, meaning that the sector
+ * is completely filled with 1's.
+ *
+ * @param[in] sector The sector to blank check.
+ * @return If the sector was blank. If an error was encountered
+ *         false is returned.
+ */
+uint8_t checkBlank( uint8_t sector ) {
+	unsigned int status = iap_blank_check( sector, sector );
+
+	return status == CMD_SUCCESS;
+}
+
+/**
+ * Compare a 4kB block of RAM with a block in flash.
+ *
+ * @param data Pointer to the start of the block in RAM.
+ * @param sector The sector where the block in flash starts.
+ * @return If the two blocks are the same. If an error was encountered
+ *         false is returned.
+ */
+uint8_t compare( uint8_t *data, uint8_t sector ) {
+	unsigned int status = iap_compare( data, sector_start_adress[sector], 4*1024 );
+
+	return status == CMD_SUCCESS;
+}
+
+/**
+ * Write a block of 4kB of data from the RAM to the flash.
+ *
+ * Make sure the flash you are going to write to is prepared and
+ * cleared if necessary.
+ *
+ * @param[in] data A pointer to the start of the data in the RAM.
+ * @param[in] sector The sector where the write starts.
+ */
+void writeFlash( uint8_t *data, uint8_t sector ) {
+	iap_write( data, sector_start_adress[sector], 4*1024 );
+}
+
+/**
+ * Get the unique serial ID of this processor.
+ *
+ * Every LPC processor has an 128 bit unique ID. This ID is unique
+ * over all LPC processors.
+ *
+ * @param[out] serial The device serial, all 16 bytes of it.
+ */
+void getDeviceSerial( uint8_t *serial ) {
+	DeviceSerial serialStruct;
+	iap_read_serial( &serialStruct );
+
+	uint8_t i=0;
+	for( i=0; i<4; i++ ) {
+		*serial = serialStruct.ser1 & 0xFF;
+		serialStruct.ser1 = serialStruct.ser1 >> 8;
+		serial++;
+	}
+	for( i=0; i<4; i++ ) {
+		*serial = serialStruct.ser2 & 0xFF;
+		serialStruct.ser2 = serialStruct.ser2 >> 8;
+		serial++;
+	}
+	for( i=0; i<4; i++ ) {
+		*serial = serialStruct.ser3 & 0xFF;
+		serialStruct.ser3 = serialStruct.ser3 >> 8;
+		serial++;
+	}
+	for( i=0; i<4; i++ ) {
+		*serial = serialStruct.ser4 & 0xFF;
+		serialStruct.ser4 = serialStruct.ser4 >> 8;
+		serial++;
+	}
+}
