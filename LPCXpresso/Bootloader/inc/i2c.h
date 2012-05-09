7651a8e25b5e3a504e50e8835a5784a65e9cbf8c
Chiel de Roest <M.A.deRoest@student.tudelft.nl>
2012-05-09 11:12:34 +0200

    Implemented initialization function for the I2C bus.

--- LPCXpresso/Bootloader/inc/i2c.h
+++ LPCXpresso/Bootloader/inc/i2c.h
@@ -18,8 +18,40 @@
 #ifndef I2C_H__
 #define I2C_H__
 
-void initI2C( uint32_t slaveAddress );
-void send( uint8_t byte );
-uint8_t receive( uint16_t address );
+/** Power Control for Peripherals bit mask */
+#define CLKPWR_PCONP_BITMASK	0xEFEFF7DE
+
+/** The I2C1 interface power/clock control bit */
+#define	 CLKPWR_PCONP_PCI2C1  	(( uint32_t )( 1<<19 ))
+
+/*******************************************************************//**
+ * I2C Control Clear register description
+ *********************************************************************/
+/** Assert acknowledge Clear bit */
+#define I2C_I2CONCLR_AAC			(( 1 << 2 ))
+/** I2C interrupt Clear bit */
+#define I2C_I2CONCLR_SIC			(( 1 << 3 ))
+/** STOP flag Clear bit  */
+#define I2C_I2CONCLR_STOC			(( 1 << 4 ))
+/** START flag Clear bit */
+#define I2C_I2CONCLR_STAC			(( 1 << 5 ))
+/** I2C interface Disable bit */
+#define I2C_I2CONCLR_I2ENC			(( 1 << 6 ))
+
+#define I2CONSET_I2EN		0x00000040  /* I2C Control Set Register */
+#define I2CONSET_AA			0x00000004
+#define I2CONSET_SI			0x00000008
+#define I2CONSET_STO		0x00000010
+#define I2CONSET_STA		0x00000020
+
+#define I2CONCLR_AAC		0x00000004  /* I2C Control clear Register */
+#define I2CONCLR_SIC		0x00000008
+#define I2CONCLR_STAC		0x00000020
+#define I2CONCLR_I2ENC		0x00000040
+
+
+void initI2C( uint32_t deviceAddress );
+void sendI2C( uint8_t byte );
+uint8_t receiveI2C();
 
 #endif
--- LPCXpresso/Bootloader/src/i2c.c
+++ LPCXpresso/Bootloader/src/i2c.c
@@ -16,14 +16,49 @@
 #include "i2c.h"
 #include "LPC17xx.h"
 
-void initI2C( uint32_t slaveAddress ) {
+/**
+ * Initializes the I2C1 peripheral.
+ * The I2C1 peripheral is set as the master device and a device using the specified address is registered as a slave device.
+ * @param[in] deviceAddress The address of the slave device on the I2C bus.
+ */
+void initI2C( uint32_t deviceAddress ) {
+
+	// Configure I/O pins
+	LPC_PINCON->PINSEL1     |=  ( 0x3 << 6 ); /* Enable SDA1 */
+	LPC_PINCON->PINSEL1     |=  ( 0x3 << 8 ); /* Enable SCL1 */
+	LPC_PINCON->PINMODE1    &= ~( 0x1 << 6 );
+	LPC_PINCON->PINMODE1    |=  ( 0x1 << 7 );
+	LPC_PINCON->PINMODE1    &= ~( 0x1 << 8 );
+	LPC_PINCON->PINMODE1    |=  ( 0x1 << 9 );
+	LPC_PINCON->PINMODE_OD0 |=  ( 0x3 << 19 );
+
+	LPC_SC->PCLKSEL1   |= ( 0x3 << 6 ); /* Select PCLK_I2C1 clock */
+ 	LPC_SC->PCONP      |= CLKPWR_PCONP_PCI2C1 & CLKPWR_PCONP_BITMASK; /* Set up clock and power for I2C1 module */
+ 	LPC_I2C1->I2CONSET  = ( I2C_I2CONCLR_AAC   | I2C_I2CONCLR_STAC |
+ 							I2C_I2CONCLR_I2ENC | I2C_I2CONCLR_SIC ); /* Initilialize CONSET register, see table 381 */
+
+ 	LPC_I2C1->I2SCLH = 16; /* Select data rate  */
+ 	LPC_I2C2->I2SCLL = 16; /* Select duty cycle */
+
+ 	LPC_I2C1->I2ADR0 = deviceAddress; /* Register the slave device */
+
+ 	LPC_I2C1->I2CONSET = I2CONSET_I2EN; /* Enable I2C1 peripheral */
 
 }
 
-void send( uint8_t byte ) {
+void deInit() {
 
 }
 
-uint8_t receive( uint16_t address ) {
+void sendI2C( uint8_t byte ) {
+
+}
+
+uint8_t receiveI2C() {
+
+	// poll for incoming messages
+
+	// return value
+
 	return 0;
 }
