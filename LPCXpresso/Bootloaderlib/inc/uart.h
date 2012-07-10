af9d44dc39973fb4b4254937285515261645da93
Chiel de Roest <M.A.deRoest@student.tudelft.nl>
2012-07-10 20:27:14 +0200

    Add UART driver to library.
    Added UART driver to support communication between programmer and host.

---
+++ LPCXpresso/Bootloaderlib/inc/uart.h
@@ -0,0 +1,27 @@
+/**
+ *     ______       _       ____  _____   ______                    _   __                       __
+ *   .' ___  |     / \     |_   \|_   _| |_   _ \                  / |_[  |                     |  ]
+ *  / .'   \_|    / _ \      |   \ | |     | |_) |   .--.    .--. `| |-'| |  .--.   ,--.    .--.| | .---.  _ .--.
+ *  | |          / ___ \     | |\ \| |     |  __'. / .'`\ \/ .'`\ \| |  | |/ .'`\ \`'_\ : / /'`\' |/ /__\\[ `/'`\]
+ *  \ `.___.'\ _/ /   \ \_  _| |_\   |_   _| |__) || \__. || \__. || |, | || \__. |// | |,| \__/  || \__., | |
+ *   `.____ .'|____| |____||_____|\____| |_______/  '.__.'  '.__.' \__/[___]'.__.' \'-;__/ '.__.;__]'.__.'[___]
+ *
+ * ===============================================================================================================
+ *
+ * The driver functions for the UART peripheral.
+ *
+ * @author Chiel de Roest <M.A.deRoest@student.tudelft.nl> and Harmjan Treep <harmjan.treep@gmail.com>
+ */
+
+#include <stdint.h>
+
+#ifndef UART_H__
+#define UART_H__
+
+void initUART();
+void deinitUART();
+void uartSend(uint8_t *data, uint32_t length);
+uint8_t* uartReceive( uint32_t length );
+void UART0_IRQHandler( void );
+
+#endif
