1d1cbd9a1decacea5f0cab7bb94526042101f2cd
Chiel de Roest <M.A.deRoest@student.tudelft.nl>
2012-07-10 20:30:24 +0200

    Add Host communication layer to programmer.
    Added Host layer for the programmer to use the UART functions.

---
+++ LPCXpresso/Programmer/inc/host.h
@@ -0,0 +1,27 @@
+/**
+ *     ______       _       ____  _____   ______                    _   __                       __
+ *   .' ___  |     / \     |_   \|_   _| |_   _ \                  / |_[  |                     |  ]
+ *  / .'   \_|    / _ \      |   \ | |     | |_) |   .--.    .--. `| |-'| |  .--.   ,--.    .--.| | .---.  _ .--.
+ *  | |          / ___ \     | |\ \| |     |  __'. / .'`\ \/ .'`\ \| |  | |/ .'`\ \`'_\ : / /'`\' |/ /__\\[ `/'`\]
+ *  \ `.___.'\ _/ /   \ \_  _| |_\   |_   _| |__) || \__. || \__. || |, | || \__. |// | |,| \__/  || \__., | |
+ *   `.____ .'|____| |____||_____|\____| |_______/  '.__.'  '.__.' \__/[___]'.__.' \'-;__/ '.__.;__]'.__.'[___]
+ *
+ * ===============================================================================================================
+ *
+ * The functions to communicate with the host.
+ *
+ * @author Chiel de Roest <M.A.deRoest@student.tudelft.nl> and Harmjan Treep <harmjan.treep@gmail.com>
+ */
+
+#include <stdint.h>
+
+#ifndef HOST_H__
+#define HOST_H__
+
+void initHost();
+void deinitHost();
+uint8_t* hostReceiveData( uint32_t length );
+uint8_t  hostReceiveCommand();
+void hostSend( uint8_t *data, uint32_t length );
+
+#endif
---
+++ LPCXpresso/Programmer/src/host.c
@@ -0,0 +1,40 @@
+/**
+ *     ______       _       ____  _____   ______                    _   __                       __
+ *   .' ___  |     / \     |_   \|_   _| |_   _ \                  / |_[  |                     |  ]
+ *  / .'   \_|    / _ \      |   \ | |     | |_) |   .--.    .--. `| |-'| |  .--.   ,--.    .--.| | .---.  _ .--.
+ *  | |          / ___ \     | |\ \| |     |  __'. / .'`\ \/ .'`\ \| |  | |/ .'`\ \`'_\ : / /'`\' |/ /__\\[ `/'`\]
+ *  \ `.___.'\ _/ /   \ \_  _| |_\   |_   _| |__) || \__. || \__. || |, | || \__. |// | |,| \__/  || \__., | |
+ *   `.____ .'|____| |____||_____|\____| |_______/  '.__.'  '.__.' \__/[___]'.__.' \'-;__/ '.__.;__]'.__.'[___]
+ *
+ * ===============================================================================================================
+ *
+ * The functions to communicate with the host.
+ *
+ * @author Chiel de Roest <M.A.deRoest@student.tudelft.nl> and Harmjan Treep <harmjan.treep@gmail.com>
+ */
+
+#include "LPC17xx.h"
+#include "host.h"
+#include "uart.h"
+
+void hostSend( uint8_t *data, uint32_t length ) {
+
+	uartSend( data, length );
+
+}
+
+uint8_t* hostReceiveData( uint32_t length ) {
+
+	return uartReceive( length );
+
+}
+
+uint8_t hostReceiveCommand( void ) {
+	return *(uartReceive( 1 ));
+}
+
+void initHost() {
+	initUART();
+}
+
+void deinitHost() {}
